9fc605e9a595a0bf9878ebda5ee465d7a082873b
ewertonbrm <88634234+ewertonbrm@users.noreply.github.com>
2025-10-01 12:46:23 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open('tinfooil-cache').then(cache => cache.addAll([
+    caches.open('tinfoil-cache').then(cache => cache.addAll([
       '/',
       '/index.html',
       '/manifest.json'
